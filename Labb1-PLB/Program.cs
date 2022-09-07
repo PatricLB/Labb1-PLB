7e36f568ddc019518b56d03b21be0d6035f33303
PatricLB <Patriclovgrenb@gmail.com>
2022-09-07 21:46:25 +0200

    Skapade metoderna "SökIgenomSträng", "LetaOchMarkeraSiffror". Påbörjade logiken

--- Labb1-PLB/Program.cs
+++ Labb1-PLB/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string exempelText = "29535123p48723487597645723645";
+
+            LetaOchMarkeraSiffror(exempelText);
+            void LetaOchMarkeraSiffror(string sökSträng)
+            {
+                SökIgenomSträng(sökSträng);
+
+            }
+
+            void SökIgenomSträng(string strängAttSöka, int aktuellSiffra = 0)
+            {
+                string tempSträng = "";
+                string klarSträng = "";
+                bool sättTillRöd = false;
+                for (int i = 0; i < strängAttSöka.Length; i++)
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    if (strängAttSöka[i].Equals('2'))
+                    {
+                        aktuellSiffra = 2;
+                        sättTillRöd = true;
+                        
+                        Console.WriteLine("aktuell position: " + i);
+                            klarSträng += strängAttSöka[i];
+                        if (klarSträng[i].Equals('2') && i !=0)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.WriteLine(klarSträng);
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.WriteLine("Hittade siffran");
+                            aktuellSiffra += 1;
+                        }
+
+                    }
+                    else
+                    {
+                        klarSträng += strängAttSöka[i];
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+
+                }
+            }
         }
+
+
+
     }
 }
